59869c2d14e651bd182c5d7aaaf19c4b2101ae15
Lawrence Kim <83670601+lawrencekim5@users.noreply.github.com>
2026-03-10 14:06:56 -0400

    Add files via upload

--- SCAMR_Helper.ps1
+++ SCAMR_Helper.ps1
@@ -2,16 +2,17 @@
 $sourceFolder = ""
 $API_URL = ""
 
+# Prompts user to enter in the file path or URL containing their source code. Removes "/" and "\" that is sometimes included when copying pasting URLs
 $sourceFolder = Read-Host "Enter the file path or URL of the root directory of your source code. Only GitHub and GitLab links are supported"
-$sourceFolder = $sourceFolder -replace '/$',''
+$sourceFolder = $sourceFolder -replace '/$', '' -replace '\$', ''
 # https://github.com/lawrencekim5/SCAMR_Helper_SampleSourceCode
 # https://api.github.com/repos/lawrencekim5/SCAMR_Helper_SampleSourceCode/contents
 
 
 
-#############################################################################
-# This part of the code handles remote source code locations (GitHub/GitLab)#
-#############################################################################
+##############################################################################
+# This part of the code handles remote source code locations (GitHub/GitLab) #
+##############################################################################
 
 # GitHub and GitLab use https so this is the easiest way to detect a URL
 
@@ -55,6 +56,7 @@ if ($sourceFolder -Match 'https://') {
         $token = Read-Host "Enter your GitLab API Token"
         
         # Authorize GitLab access through token in header
+        # FIXME - add some way to take this in as a secure string
         $headers = @{          
             "PRIVATE-TOKEN" = "$token"
         }
@@ -67,7 +69,7 @@ if ($sourceFolder -Match 'https://') {
 
     # Exit on invalid input
     else {
-        Read-Host "Not a valid GitHub or GitLab link. Exiting script... Press any key to exit."
+        Read-Host "Not a valid GitHub or GitLab link. Exiting script. Press any key to exit"
         exit
     }
 
@@ -91,12 +93,27 @@ if ($sourceFolder -Match 'https://') {
         }
     }
 
+    # Notifies the user that the URL was inaccessible
     catch {
         ""
-        Read-Host "An Error occured when attempting to access the URL. Please check your input and try again."
-        ""
         "ERROR MESSAGE: $($Error[0])"
-        exit
+        ""
+        Write-Host "UNABLE TO DISPLAY DIRECTORY INFORMATION"
+        $confirmation = Read-Host "An Error occured when attempting to access the URL. Do you wish to continue regardless? [y/n]"
+        if ($confirmation -eq 'y') {
+                Write-Host "Continuing script..."
+            }
+
+            # Exit code if [y] is not the input
+            elseif ($confirmation -eq 'n') {
+                Read-Host "Exiting script. Please rerun the script to try again. Press any key to exit"
+                exit
+            }
+
+            else {
+                Read-Host "Invalid input. Exiting script. Press any key to exit"
+                exit
+            }
     }
 
 
@@ -154,12 +171,12 @@ if ($sourceFolder -Match 'https://') {
 
         # Exit code if [y] is not the input
         elseif ($confirmation -eq 'n') {
-            Read-Host "Exiting script. Please rerun the script to try again. Press any key to exit."
+            Read-Host "Exiting script. Please rerun the script to try again. Press any key to exit"
             exit
         }
 
         else {
-            Read-Host "Invalid input. Exiting script... Press any key to exit."
+            Read-Host "Invalid input. Exiting script. Press any key to exit"
             exit
         }
             
@@ -168,9 +185,9 @@ if ($sourceFolder -Match 'https://') {
 
 
 
-#####################################################################################
-# This part of the code handles local source code locations (integrates with VSCode)#
-#####################################################################################
+######################################################################################
+# This part of the code handles local source code locations (integrates with VSCode) #
+######################################################################################
 
 else {
     $type = 'local'
@@ -190,37 +207,48 @@ else {
                 Write-Host "Confirmed"
             }
 
-        # Exit code if [y] is not the input
+            # Exit code if [y] is not the input
             elseif ($confirmation -eq 'n') {
-                Read-Host "Exiting script. Please rerun the script to try again. Press any key to exit."
+                Read-Host "Exiting script. Please rerun the script to try again. Press any key to exit"
                 exit
             }
 
             else {
-                Read-Host "Invalid input. Exiting script... Press any key to exit."
+                Read-Host "Invalid input. Exiting script. Press any key to exit"
                 exit
             }
-            
-        
     }
 
 
     # Error Catching for invalid file paths
     catch {
         ""
-        Write-Host "An Error occured. Please check your input and try again."
-        ""
         "ERROR MESSAGE: $($Error[0])"
-        "Press any key to exit."
-        exit
+        ""
+        Write-Host "UNABLE TO DISPLAY DIRECTORY INFORMATION"
+        $confirmation = Read-Host "Unable to find the path on your local machine. Do you wish to continue regardless? [y/n]"
+        if ($confirmation -eq 'y') {
+                Write-Host "Continuing script..."
+            }
+
+            # Exit code if [y] is not the input
+            elseif ($confirmation -eq 'n') {
+                Read-Host "Exiting script. Please rerun the script to try again. Press any key to exit"
+                exit
+            }
+
+            else {
+                Read-Host "Invalid input. Exiting script. Press any key to exit"
+                exit
+            }
     }
 }
 
 
 
-###############################
-# CSV Parsing and Modification#
-###############################
+################################
+# CSV Parsing and Modification #
+################################
 
 Write-Host "Select the CSV file to be modified."
 
@@ -242,7 +270,7 @@ try {
 
 # Error handling for improper CSV files
 catch {
-    Write-Host "An error occured. Please check whether the input CSV file is valid. Exiting script... Press any key to exit."
+    Write-Host "An error occured. Please check whether the input CSV file is valid. Exiting script. Press any key to exit"
     exit
 }
 
@@ -251,10 +279,10 @@ catch {
 # $columns = ($rawCSVdata | Get-Member -MemberType NoteProperty).Name
 # Write-Host $columns
 
-
+###############
 # This section of code extracts the name of the column header that is used for filepaths in the CSV.
-
-# Creates an array storing the values underneath each column header
+###############
+# Creates an array storing the values underneath each column header. This is surprisingly robust as long as if the filepath does not contain semicolons
 $CSVdatafields = $rawCSVdata[0] -split ";"
 
 # converting sourcePath to the minimum necessary for identification for compatibility
@@ -275,7 +303,7 @@ foreach ($item in $CSVdatafields) {
 # Error handing incase source path can not be identified in the CSV file
 if ($filenameHeader -eq '') {
     Write-Host "Source code path was unable to be identified in the CSV file. Please check the file and make sure that it matches your source code repository location."
-    Read-Host "Exiting script... Press any key to exit."
+    Read-Host "Exiting script. Press any key to exit"
     exit
 }
 
@@ -289,99 +317,57 @@ foreach ($item in $rawCSVdata) {
     # extract relevant filepath and filename, extract line number, add columns to include hyperlink quick access to code, hyperlink quick access to cwe, and then hyperlink helper columns
     
 
-    # This section of code should identify excel file headers
-
-
-
+    # Some variables to track the values of the current item. Just makes the script more readable.
     $item_filename = $($item.$filenameHeader)
     $item_line = $($item.line)
     $item_cwe = $($item.cwe)
-    Write-Host $item_filename
-    Write-Host $item_line
-    Write-Host $item_cwe
-    
-    ##############################################
-    # Hyperlink creation based on repository type#
-    ##############################################
+
+    # Displays loop progress so that users don't think the script is hanging
+    $loopCounter = $rawCSVdata.IndexOf($item) + 1
+    Write-Host "$loopCounter item(s) identified!"
+    Write-Host "File name:   $item_filename"
+    Write-Host "Line number: $item_line"
+    Write-Host "CWE:         $item_cwe"
+    ""
+        
+    ###############################################
+    # Hyperlink creation based on repository type #
+    ###############################################
 
 
     if ($item_filename -ne '') {
 
         # Local Hyperlinks
         if ($type -eq 'local') {
-
-        
             $hyperlink_notClickable = "vscode://file/" + $item_filename + ":" + $item_line
-
             $hyperlink_filename = $hyperlink_notClickable -replace '.*\\', ''
-
             $hyperlink_clickable = "=HYPERLINK(`"" + $hyperlink_notClickable + "`", `"" + $hyperlink_filename + "`")"
-
-            <# Testing
-            Write-Host "PUT IN EXCEL"
-            Write-Host $hyperlink_notClickable
-            Write-Host $hyperlink_filename
-            Write-Host $hyperlink_clickable
-            #>
         }
 
 
         # GitHub Hyperlinks
         elseif ($type -eq 'github') {
-        
-            # https://github.com/lawrencekim5/SCAMR_Helper_SampleSourceCode/blob/main/{path}
-
-            # has to look like this
-            # https://github.com/lawrencekim5/SCAMR_Helper_SampleSourceCode/blob/main/Objects/clinic/listobject.c.h#L8
-        
-            Write-Host "source path is:" $sourcePath
-            Write-Host "hyperlink base is:" $hyperlink_base
-        
             $hyperlink_minusSourcePath = $item_filename -replace ".*$sourcePath", '' -replace '^/', ''
-
             $hyperlink_notClickable = $hyperlink_base + $hyperlink_minusSourcePath + "#L" + $item_line
-
             $hyperlink_filename = $hyperlink_notClickable -replace '.*\/', ''
-
             $hyperlink_clickable = "=HYPERLINK(`"" + $hyperlink_notClickable + "`", `"" + $hyperlink_filename + "`")"
-
-            <# Testing
-            Write-Host "PUT IN EXCEL"
-            Write-Host $hyperlink_minusSourcePath
-            Write-Host $hyperlink_notClickable
-            Write-Host $hyperlink_filename
-            Write-Host $hyperlink_clickable
-            #>
         }
 
 
         # GitLab Hyperlinks
         elseif ($type -eq 'gitlab') {
-
-        
-            Write-Host "source path is:" $sourcePath
-            Write-Host "hyperlink base is:" $hyperlink_base
-        
             $hyperlink_minusSourcePath = $item_filename -replace ".*$sourcePath", '' -replace '^/', ''
-
             $hyperlink_notClickable = $hyperlink_base + $hyperlink_minusSourcePath + "#L" + $item_line
-
             $hyperlink_filename = $hyperlink_notClickable -replace '.*\/', ''
-
             $hyperlink_clickable = "=HYPERLINK(`"" + $hyperlink_notClickable + "`", `"" + $hyperlink_filename + "`")"
-
-            <# Testing
-            Write-Host "PUT IN EXCEL"
-            Write-Host $hyperlink_minusSourcePath
-            Write-Host $hyperlink_notClickable
-            Write-Host $hyperlink_filename
-            Write-Host $hyperlink_clickable
-            #>
         }
 
         else {
             exit
         }
+        
+
+        # Hyperlinking to MITRE CWEs
 
 
     }
@@ -400,18 +386,41 @@ foreach ($item in $rawCSVdata) {
     
     # CSV Modification code
 
-    <#
-    $item | Add-Member -NotePropertyName 'Hyperlink_to_Code' -NotePropertyValue $($hyperlink_clickable.Hyperlink_to_Code)
-    Write-Host "HYPERLINK TO CODE" $($hyperlink_clickable.Hyperlink_to_Code)
-    #>
-    $item | Add-Member -MemberType NoteProperty -Name "Hyperlink to Finding" -Value "$hyperlink_clickable"
-    $rawCSVdata | Export-CSV "updated.csv" -NoTypeInformation
+    # Appends a column with the hyperlink to each object in the $rawCSVdata array
+    $item | Add-Member -MemberType NoteProperty -Name "Link to Finding" -Value "$hyperlink_clickable"
+    
+    # Creates MITRE hyperlink based off of the CWE identifier
+    $MITRE_hyperlink = "=HYPERLINK(`"https://cwe.mitre.org/data/definitions/" + $item_cwe + ".html`"" + ", `"CWE: " + $item_cwe + "`")"
 
-
-
-    # $rawCSVdata += Add-Member -InputObject $user -NotePropertyName ADEndDate -NotePropertyValue $ADEndDate.AccountExpirationDate
-
+    # Appends a column with the hyperlink to the MITRE CWE site for each object in the $rawCSVdata array if an associated CWE exists
+    if ($item_cwe -ne '') {
+        $item | Add-Member -MemberType NoteProperty -Name "Link to MITRE CWE" -Value "$MITRE_hyperlink"
+    }
+    
     
 }
-$ModifiedCSVfilename = Read-Host "Modifications complete! Enter a name to save the modifed CSV file as"
-Export-CSV $ModifiedCSVfilename -NotypeInformation
+
+# While iterates stays until an acceptable file name is given. This ensures that the file is saved with a valid name.
+$fileSaveSuccess = 'False'
+while ($fileSaveSuccess -eq 'False') {
+    # Removes ".csv" extension if it is already provided, adds .csv extension afterwards
+    $ModifiedCSVfilename = Read-Host "Enter a name to save the modifed CSV file as"
+    $ModifiedCSVfilename = $ModifiedCSVfilename -replace '\.csv$',''
+    $ModifiedCSVfilename += '.csv'
+    try {
+        $rawCSVdata | Export-CSV "$ModifiedCSVfilename" -NoTypeInformation
+        $fileSaveSuccess = 'True'
+        break
+    }
+    catch {
+        ""
+        "ERROR MESSAGE: $($Error[0])"
+        ""
+        Write-Host "An error occured when attempting to save the file name. Please check and try again."
+    }
+}
+
+
+Read-Host "Modifications complete! File saved as $ModifiedCSVfilename. Press any key to exit"
+
+
